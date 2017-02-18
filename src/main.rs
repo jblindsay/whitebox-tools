3e3646452f34817c79ea3ce7f6980e9e71e638c7
John Lindsay <jlindsay@uoguelph.ca>
2017-02-18 16:21:10 -0500

    Added RMSE tool
    Added RMSE tool

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+// use std::path;
+
+fn main() {
+    // let sep: String = path::MAIN_SEPARATOR.to_string();
+    let mut input_file: String;
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 { panic!("Tool run with no paramters. Please see help (-h) for parameter descriptions."); }
+    for i in 0..args.len() {
+        let mut arg = args[i].replace("\"", "");
+        arg = arg.replace("\'", "");
+        let cmd = arg.split("="); // in case an equals sign was used
+        let vec = cmd.collect::<Vec<&str>>();
+        let mut keyval = false;
+        if vec.len() > 1 { keyval = true; }
+        if vec[0].to_lowercase() == "-i" || vec[0].to_lowercase() == "--i" {
+            if keyval {
+                input_file = vec[1].to_string();
+            } else {
+                input_file = args[i+1].to_string();
+            }
+            println!("{}", input_file);
+        }
+        println!("{}", vec[0]);
+    }
+    println!("Complete!");
+}
