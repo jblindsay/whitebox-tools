946af19162e11a2fc55f55af0d66e22f9dc7b5e6
John Lindsay <jlindsay@uoguelph.ca>
2018-01-31 10:24:50 -0500

    HypsometricAnalysis tool now handles watershed inputs
    HypsometricAnalysis tool now handles watershed inputs

--- src/tools/terrain_analysis/hypsometric_analysis.rs
+++ src/tools/terrain_analysis/hypsometric_analysis.rs
@@ -36,14 +36,6 @@ impl HypsometricAnalysis {
         let description = "Calculates a hypsometric curve for one or more DEMs.".to_string();
 
         let mut parameters = vec![];
-        // parameters.push(ToolParameter{
-        //     name: "Input DEM File".to_owned(), 
-        //     flags: vec!["-i".to_owned(), "--input".to_owned()], 
-        //     description: "Input DEM file.".to_owned(),
-        //     parameter_type: ParameterType::ExistingFile(ParameterFileType::Raster),
-        //     default_value: None,
-        //     optional: false
-        // });
         parameters.push(ToolParameter{
             name: "Input DEM Files".to_owned(), 
             flags: vec!["-i".to_owned(), "--inputs".to_owned()], 
@@ -51,6 +43,15 @@ impl HypsometricAnalysis {
             parameter_type: ParameterType::FileList(ParameterFileType::Raster),
             default_value: None,
             optional: false
+        });
+
+        parameters.push(ToolParameter{
+            name: "Input Watershed Files (optional)".to_owned(), 
+            flags: vec!["--watershed".to_owned()], 
+            description: "Input watershed files (optional).".to_owned(),
+            parameter_type: ParameterType::FileList(ParameterFileType::Raster),
+            default_value: None,
+            optional: true
         });
 
         parameters.push(ToolParameter{
@@ -72,7 +73,7 @@ impl HypsometricAnalysis {
         if e.contains(".exe") {
             short_exe += ".exe";
         }
-        let usage = format!(">>.*{0} -r={1} -v --wd=\"*path*to*data*\" -i=\"DEM.tif\" -o=outfile.html",
+        let usage = format!(">>.*{0} -r={1} -v --wd=\"*path*to*data*\" -i=\"DEM1.tif;DEM2.tif\" --watershed=\"ws1.tif;ws2.tif\" -o=outfile.html",
                             short_exe, name).replace("*", &sep);
 
         HypsometricAnalysis {
@@ -125,8 +126,8 @@ impl WhiteboxTool for HypsometricAnalysis {
                working_directory: &'a str,
                verbose: bool)
                -> Result<(), Error> {
-        // let mut input_file = String::new();
         let mut input_files_str = String::new();
+        let mut watershed_files_str = "".to_string();
         let mut output_file = String::new();
 
         if args.len() == 0 {
@@ -142,14 +143,14 @@ impl WhiteboxTool for HypsometricAnalysis {
                 keyval = true;
             }
             let flag_val = vec[0].to_lowercase().replace("--", "-");
-            // if flag_val == "-i" || flag_val == "-input" {
-            //     if keyval {
-            //         input_file = vec[1].to_string();
-            //     } else {
-            //         input_file = args[i+1].to_string();
-            //     }
             if flag_val == "-i" || flag_val == "-inputs" {
                 input_files_str = if keyval {
+                    vec[1].to_string()
+                } else {
+                    args[i+1].to_string()
+                };
+            } else if flag_val == "-watershed" {
+                watershed_files_str = if keyval {
                     vec[1].to_string()
                 } else {
                     args[i+1].to_string()
@@ -185,7 +186,7 @@ impl WhiteboxTool for HypsometricAnalysis {
         let num_files = input_files.len();
         if num_files < 1 {
             return Err(Error::new(ErrorKind::InvalidInput,
-                                "There is something incorrect about the input files. At least two inputs are required to operate this tool."));
+                                "There is something incorrect about the input DEM files. At least one input DEM is required to operate this tool."));
         }
 
         if !output_file.contains(&sep) {
@@ -214,68 +215,213 @@ impl WhiteboxTool for HypsometricAnalysis {
         let mut xdata = vec![];
         let mut ydata = vec![];
         let mut shortnames = vec![];
-        for i in 0..num_files {
-            let mut input_file = input_files[i].to_string();
-            if !input_file.contains(&sep) {
-                input_file = format!("{}{}", working_directory, input_file);
-            }
-            let input = Raster::new(&input_file, "r")?;
 
-            if num_files == 1 {
-                writer.write_all((format!("<p><strong>Input DEM</strong>: {}", input.get_short_filename())).as_bytes())?;
-            }
-            let rows = input.configs.rows as isize;
-            let columns = input.configs.columns as isize;
-            let nodata = input.configs.nodata;
-            
-            let min = input.configs.minimum;
-            let max = input.configs.maximum;
-            let range = max - min + 0.00001f64;
-            let mut num_bins = (max - min) as usize / 5;
-            if num_bins < ((rows * columns) as f64).log2().ceil() as usize  + 1 {
-                num_bins = ((rows * columns) as f64).log2().ceil() as usize  + 1;
-            }
-            let bin_width = range / num_bins as f64;
-            let mut freq_data = vec![0f64; num_bins];
-            let mut bin_elevations = vec![0f64; num_bins];
-            let mut val: f64;
-            let mut bin: usize;
-            let mut total_n = 0f64;
-            for row in 0..rows {
-                for col in 0..columns {
-                    val = input.get_value(row, col);
-                    if val != nodata {
-                        bin = ((val - min) / bin_width).floor() as usize;
-                        freq_data[bin] += 1f64;
-                        total_n += 1f64;
+        if watershed_files_str.is_empty() {
+            for i in 0..num_files {
+                let mut input_file = input_files[i].to_string();
+                if !input_file.contains(&sep) {
+                    input_file = format!("{}{}", working_directory, input_file);
+                }
+                let input = Raster::new(&input_file, "r")?;
+
+                if num_files == 1 {
+                    writer.write_all((format!("<p><strong>Input DEM</strong>: {}", input.get_short_filename())).as_bytes())?;
+                }
+                let rows = input.configs.rows as isize;
+                let columns = input.configs.columns as isize;
+                let nodata = input.configs.nodata;
+                
+                let min = input.configs.minimum;
+                let max = input.configs.maximum;
+                let range = max - min + 0.00001f64;
+                let mut num_bins = (max - min) as usize / 5;
+                if num_bins < ((rows * columns) as f64).log2().ceil() as usize  + 1 {
+                    num_bins = ((rows * columns) as f64).log2().ceil() as usize  + 1;
+                }
+                let bin_width = range / num_bins as f64;
+                let mut freq_data = vec![0f64; num_bins];
+                let mut bin_elevations = vec![0f64; num_bins];
+                let mut val: f64;
+                let mut bin: usize;
+                let mut total_n = 0f64;
+                for row in 0..rows {
+                    for col in 0..columns {
+                        val = input.get_value(row, col);
+                        if val != nodata {
+                            bin = ((val - min) / bin_width).floor() as usize;
+                            freq_data[bin] += 1f64;
+                            total_n += 1f64;
+                        }
+                    }
+                    if verbose {
+                        progress = (100.0_f64 * row as f64 / (rows - 1) as f64) as usize;
+                        if progress != old_progress {
+                            println!("Binning the data: {}%", progress);
+                            old_progress = progress;
+                        }
                     }
                 }
-                if verbose {
-                    progress = (100.0_f64 * row as f64 / (rows - 1) as f64) as usize;
-                    if progress != old_progress {
-                        println!("Binning the data: {}%", progress);
-                        old_progress = progress;
-                    }
+
+                bin_elevations[0] = min;
+                for i in 1..num_bins {
+                    freq_data[i] += freq_data[i - 1];
+                    bin_elevations[i] = min + i as f64 * bin_width;
+                }
+
+                for i in 0..num_bins {
+                    freq_data[i] = 100f64 * (1f64 - freq_data[i] / total_n);
+                }
+
+                freq_data[num_bins-1] = 0.0001; // this is necessary so the axis will start at zero.
+                xdata.push(freq_data);
+                ydata.push(bin_elevations);
+                shortnames.push(input.get_short_filename());
+
+                if num_files > 1 {
+                    writer.write_all(&format!("{}<br>", shortnames[i]).as_bytes())?;
                 }
             }
-
-            bin_elevations[0] = min;
-            for i in 1..num_bins {
-                freq_data[i] += freq_data[i - 1];
-                bin_elevations[i] = min + i as f64 * bin_width;
+        } else {
+            // there are watersheds specified for each input DEM.
+            let mut cmd = watershed_files_str.split(";");
+            let mut watershed_files = cmd.collect::<Vec<&str>>();
+            if watershed_files.len() == 1 {
+                cmd = watershed_files_str.split(",");
+                watershed_files = cmd.collect::<Vec<&str>>();
+            }
+            if watershed_files.len() != num_files {
+                return Err(Error::new(ErrorKind::InvalidInput,
+                        "There should be the same number of input DEM and watershed rasters, if watersheds are used."));
             }
 
-            for i in 0..num_bins {
-                freq_data[i] = 100f64 * (1f64 - freq_data[i] / total_n);
-            }
+            for i in 0..num_files {
+                let mut input_file = input_files[i].to_string();
+                if !input_file.contains(&sep) {
+                    input_file = format!("{}{}", working_directory, input_file);
+                }
+                let input = Raster::new(&input_file, "r")?;
+                if num_files == 1 {
+                    writer.write_all((format!("<p><strong>Input DEM</strong>: {}", input.get_short_filename())).as_bytes())?;
+                }
+                let rows = input.configs.rows as isize;
+                let columns = input.configs.columns as isize;
+                let nodata = input.configs.nodata;
+                
+                let mut watershed_file = watershed_files[i].to_string();
+                if !watershed_file.contains(&sep) {
+                    watershed_file = format!("{}{}", working_directory, watershed_file);
+                }
+                let watershed = Raster::new(&watershed_file, "r")?;
+                let ws_nodata = watershed.configs.nodata;
+                if watershed.configs.rows as isize != rows || watershed.configs.columns as isize != columns {
+                    return Err(Error::new(ErrorKind::InvalidInput,
+                            "The input DEM and watershed rasters should have the same extents (rows and columns)."));
+                }
 
-            freq_data[num_bins-1] = 0.0001; // this is necessary so the axis will start at zero.
-            xdata.push(freq_data);
-            ydata.push(bin_elevations);
-            shortnames.push(input.get_short_filename());
+                let watershed_min = watershed.configs.minimum;
+                let watershed_max = watershed.configs.maximum;
+                let num_watersheds = (watershed_max - watershed_min) as usize + 1;
 
-            if num_files > 1 {
-                writer.write_all(&format!("{}<br>", shortnames[i]).as_bytes())?;
+                // get the number of watersheds, and the min and max elev for each watershed
+                let mut min_elevs = vec![f64::INFINITY; num_watersheds];
+                let mut max_elevs = vec![f64::NEG_INFINITY; num_watersheds];
+                let mut z: f64;
+                let mut val: f64;
+                let mut watershed_id: usize;
+                for row in 0..rows {
+                    for col in 0..columns {
+                        z = input.get_value(row, col);
+                        if z != nodata {
+                            val = watershed.get_value(row, col);
+                            if val != 0f64 && val != ws_nodata {
+                                watershed_id = (val - watershed_min) as usize;
+                                if z < min_elevs[watershed_id] { min_elevs[watershed_id] = z; }
+                                if z > max_elevs[watershed_id] { max_elevs[watershed_id] = z; }
+                            }
+                        }
+                    }
+                    if verbose {
+                        progress = (100.0_f64 * row as f64 / (rows - 1) as f64) as usize;
+                        if progress != old_progress {
+                            println!("Processing watershed data: {}%", progress);
+                            old_progress = progress;
+                        }
+                    }
+                }
+
+                let mut freq_data = vec![];
+                let mut bin_elevations = vec![];
+                let mut num_bins_data = vec![];
+                let mut bin_widths = vec![];
+                for w in 0..num_watersheds {
+                    if min_elevs[w] < f64::INFINITY {
+                        let min = min_elevs[w];
+                        let max = max_elevs[w];
+                        let range = max - min + 0.00001f64;
+                        let mut num_bins = (max - min) as usize / 5;
+                        if num_bins < ((rows * columns) as f64).log2().ceil() as usize  + 1 {
+                            num_bins = ((rows * columns) as f64).log2().ceil() as usize  + 1;
+                        }
+                        let bin_width = range / num_bins as f64;
+                        bin_widths.push(bin_width);
+                        freq_data.push(vec![0f64; num_bins]);
+                        num_bins_data.push(num_bins);
+                        bin_elevations.push(vec![0f64; num_bins]);
+                    } else {
+                        freq_data.push(vec![0f64; 1]);
+                        bin_elevations.push(vec![0f64; 1]);
+                        num_bins_data.push(0);
+                        bin_widths.push(0f64);
+                    }
+                }
+                
+                let mut bin: usize;
+                let mut total_n = vec![0f64; num_watersheds];
+                for row in 0..rows {
+                    for col in 0..columns {
+                        z = input.get_value(row, col);
+                        if z != nodata {
+                            val = watershed.get_value(row, col);
+                            if val != 0f64 && val != ws_nodata {
+                                watershed_id = (val - watershed_min) as usize;
+                                bin = ((z - min_elevs[watershed_id]) / bin_widths[watershed_id]).floor() as usize;
+                                freq_data[watershed_id][bin] += 1f64;
+                                total_n[watershed_id] += 1f64;
+                            }
+                        }
+                    }
+                    if verbose {
+                        progress = (100.0_f64 * row as f64 / (rows - 1) as f64) as usize;
+                        if progress != old_progress {
+                            println!("Binning the data: {}%", progress);
+                            old_progress = progress;
+                        }
+                    }
+                }
+
+                for w in 0..num_watersheds {
+                    if min_elevs[w] < f64::INFINITY {
+                        bin_elevations[w][0] = min_elevs[w];
+                        for i in 1..num_bins_data[w] {
+                            freq_data[w][i] += freq_data[w][i - 1];
+                            bin_elevations[w][i] = min_elevs[w] + i as f64 * bin_widths[w];
+                        }
+
+                        for i in 0..num_bins_data[w] {
+                            freq_data[w][i] = 100f64 * (1f64 - freq_data[w][i] / total_n[w]);
+                        }
+
+                        freq_data[w][num_bins_data[w]-1] = 0.0001; // this is necessary so the axis will start at zero.
+                        xdata.push(freq_data[w].clone());
+                        ydata.push(bin_elevations[w].clone());
+                        shortnames.push(format!("{} {}", input.get_short_filename(), w as f64 + watershed_min));
+                 
+                    }
+                }   
+
+                if num_files > 1 {
+                    writer.write_all(&format!("{}<br>", shortnames[i]).as_bytes())?;
+                }
             }
         }
         writer.write_all(("</p>").as_bytes())?;
